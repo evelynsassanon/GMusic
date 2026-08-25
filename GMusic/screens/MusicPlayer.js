76f268f368ab2bce7d64a80b73f3172816bd9984
evelynsassanon <borgesevelyn3280@gmail.com>
2026-08-25 12:23:46 -0300

    para com isso, novinha vem vem

--- GMusic/screens/MusicPlayer.js
+++ GMusic/screens/MusicPlayer.js
@@ -1,10 +1,25 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, FlatList, Image, useWindowDimensions } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context';
-
+import colors from '../theme/colors'
+import { songs } from '../model/data'
 import colors from '../theme/colors';
 
 export default function MusicPlayer() {
+  const { widht } = useWindowDimensions ();
+  const [selctedIndex, setSelectedIndex] = useState(0);
+
+  const currentSong = songs[selectedIndex];
+  const artworkSize = Math.min(widht - 40, 380);
+
+  function handleMomentumEnd(event) {
+    const offset = event.nativeEvent.contentOffset.x;
+    const index = Math.round(offset / widht);
+    setSelectedIndex(index);
+  }
+
+
+  
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.content}>
